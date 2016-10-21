50bf273b49e66f04db8a9baa0e81ac87a04d1e62
Jonathan Thom <jonathan.thom1990@gmail.com>
2016-10-21 09:44:38 -0700

    Fix results of track outcome

--- js/scripts.js
+++ js/scripts.js
@@ -1,18 +1,21 @@
 //Business Logic
 
-//define empty variables??
+var track;
 
 var whatTrack = function(answer1, answer2, answer3) {
   if (answer1 === "works" && answer2 === "newrelic" && answer3 === "startup") {
-    return "Ruby";
+    track = "Ruby";
   }
-  else if (answer1 === "works" && answer2 === "newrelic" && answer3 === "corporate") {
-    return "C#/.NET";
+  else if (answer3 === "corporate") {
+    track = "C#/.NET";
   }
   else {
-    return "Design";
+    track = "Design";
   }
+  return track;
 }
+
+//User Interface Logic
 
 $(document).ready(function() {
   $("form").submit(function(event){
@@ -22,21 +25,20 @@ $(document).ready(function() {
     var answer2 = $("input:radio[name=question2]:checked").val();
     var answer3 = $("input:radio[name=question3]:checked").val();
 
-    console.log("this is answer1 " + answer1);
-    console.log("this is answer2 " + answer2);
-    console.log("this is answer3 " + answer3);
-
     var trackOutcome = whatTrack(answer1, answer2, answer3);
 
     if (trackOutcome === "Ruby") {
       $("#ruby").show();
     }
     else if (trackOutcome === "C#/.NET") {
-      $("csharp").show();
+      $("#csharp").show();
     }
     else if (trackOutcome === "Design") {
-      $("design").show();
+      $("#design").show();
     }
+
+    var nameInput = $("input#name").val();
+    $(".name").text(nameInput);
 
   });
 });
